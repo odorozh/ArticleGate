97db92751121d2e980e0c8d76df8bc9ab4969531
odorozh <oly.dorosh@gmail.com>
2025-05-01 16:00:49 +0300

    Application GET-methods

--- src/app/main.py
+++ src/app/main.py
@@ -3,10 +3,49 @@
     of the Web service 'Article Gate'.
 """
 
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import(
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+)
+from .models.base import BaseModel
+from .models.article import ArticleModel
+from .models.author import AuthorModel
+from .models.organisation import OrganisationModel
+
+from .schemas import (
+    AuthorGetSchema,
+    ArticleGetSchema,
+    OrganisationGetSchema,
+)
 
 
+db_engine = create_async_engine("sqlite+aiosqlite:///app/article_gate.sqlite3")
+new_session = async_sessionmaker(db_engine, expire_on_commit=False)
 app = FastAPI()
+
+@app.on_event("startup")
+async def setup_models():
+    async with db_engine.begin() as conn:
+        await conn.run_sync(BaseModel.metadata.create_all)
+
+
+async def make_new_session():
+    """
+        Asynchronously get new session to DB.
+    """
+    async with new_session() as session:
+        yield session
+
+
+# Dependency injection of make_new_session to avoid
+# its explicit calls in handlers.
+SessionDep = Annotated[AsyncSession, Depends(make_new_session)]
+
 
 @app.get("/")
 async def root():
@@ -19,28 +58,34 @@ async def root():
     return {"ServiceInfo": welcome_msg}
 
 
-@app.get("/author/{author_id}")
-async def get_author(author_id: int):
+@app.get("/author")
+async def get_author(data: Annotated[AuthorGetSchema, Depends()], session: SessionDep):
     """
         Handler for author information requests.
     """
 
-    return {"id": author_id, "name": "Alice"}  # TODO: change
+    query = select(AuthorModel).where(AuthorModel.id == data.id)
+    results = await session.execute(query)
+    return results.scalar()
 
 
-@app.get("/article/{doi}")
-async def get_article(doi: str):
+@app.get("/article")
+async def get_article(data: Annotated[ArticleGetSchema, Depends()], session: SessionDep):
     """
         Handler for article information requests.
     """
 
-    return {"doi": doi}  # TODO: change
+    query = select(ArticleModel).where(ArticleModel.doi == data.doi)
+    results = await session.execute(query)
+    return results.scalar()
 
 
-@app.get("/org/{org_id}")
-async def get_org(org_id: int):
+@app.get("/org")
+async def get_org(data: Annotated[OrganisationGetSchema, Depends()], session: SessionDep):
     """
         Handler for organisation information requests.
     """
 
-    return {"org_id": org_id}  # TODO: change
+    query = select(OrganisationModel).where(OrganisationModel.id == data.id)
+    results = await session.execute(query)
+    return results.scalar()
