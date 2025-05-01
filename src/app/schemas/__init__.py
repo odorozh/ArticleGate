0003cd7d6237d471a2c683ccdcff17eb2f77a381
odorozh <oly.dorosh@gmail.com>
2025-05-01 15:59:44 +0300

    Get-schemas added

---
+++ src/app/schemas/__init__.py
@@ -0,0 +1,28 @@
+"""
+    Pydentic schemas for handlers parameters.
+"""
+
+from pydantic import BaseModel as PDBaseModel, ValidationError, field_validator
+
+
+class IdGetSchema(PDBaseModel):
+    id: int
+
+    @field_validator('id', mode='after')
+    @classmethod
+    def ge(cls, value: int) -> int:
+        if value < 0:
+            raise ValueError(f'{value} is less than zero')
+        return value
+
+
+class AuthorGetSchema(IdGetSchema):
+    pass
+
+
+class ArticleGetSchema(PDBaseModel):
+    doi: str
+
+
+class OrganisationGetSchema(IdGetSchema):
+        pass
