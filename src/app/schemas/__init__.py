ca7ea0703c32dfb43a472ffbd41c528d45638dd6
odorozh <oly.dorosh@gmail.com>
2025-05-04 08:32:52 +0300

    Full schemas added

--- src/app/schemas/__init__.py
+++ src/app/schemas/__init__.py
@@ -2,10 +2,15 @@
     Pydentic schemas for handlers parameters.
 """
 
-from pydantic import BaseModel as PDBaseModel, ValidationError, field_validator
+import datetime
+from pydantic import BaseModel as PDBaseModel, field_validator
 
 
 class IdGetSchema(PDBaseModel):
+    """
+        Basic id > 0 validation schema.
+    """
+
     id: int
 
     @field_validator('id', mode='after')
@@ -17,18 +22,34 @@ class IdGetSchema(PDBaseModel):
 
 
 class AuthorIdSchema(IdGetSchema):
+    """
+        General author identification schema.
+    """
+
     pass
 
 
 class ArticleDOISchema(PDBaseModel):
+    """
+        General article identification schema.
+    """
+
     doi: str
 
 
 class OrganisationIdSchema(IdGetSchema):
+    """
+        General organisation identification schema.
+    """
+
     pass
 
 
 class ArticleAuthorBindingSchema(PDBaseModel):
+    """
+        Article to author schema for delete handler purpose.
+    """
+
     doi: str
     place: int
 
@@ -38,3 +59,40 @@ class ArticleAuthorBindingSchema(PDBaseModel):
         if place < 1:
             raise ValueError(f'{place} is less than 1')
         return place
+
+
+class ArticleFullSchema(PDBaseModel):
+    """
+        Article schema: all fields.
+    """
+
+    doi: str
+    title: str
+    posting_date: str
+
+    @field_validator('posting_date', mode='after')
+    @classmethod
+    def validate_place(cls, pd: str) -> int:
+        try:
+            datetime.datetime.strptime(pd, "%Y-%m-%d")
+            return pd
+        except Exception as _:
+            raise ValueError(f'Wrong posting date format')
+
+
+class OrganisationFullSchema(IdGetSchema):
+    """
+        Organisation schema: all fields.
+    """
+
+    title: str
+    location: str
+
+
+class AuthorFullSChema(IdGetSchema):
+    """
+        Author schema: all fields.
+    """
+
+    name: str
+    affiliation_org_id: int
